7d53a009bbac0979d72ab5e6f4d6e61293c50211
Brian Baugher <bdb7hq@virginia.edu>
2024-11-09 15:59:02 -0500

    article comment addition working
    can PUT to the comments of an article
    be sure to include changes from
    /etc/apache2/sites-enabled/001-bugle.conf

--- back/article/article.js
+++ back/article/article.js
@@ -117,3 +117,29 @@ app.put('/', async (req, res) => {
             });
         });
 })
+
+app.put('/comment', async (req, res) => {
+    console.log('Comment PUT recieved with req body: ' + JSON.stringify(req.body) + '\n')
+    if(!req.query?.id){
+        res.status(400).send({
+            message: "no ID included in PUT"
+        })
+        return;
+    }
+    const filter = {_id: new ObjectId(req.query.id)};
+    const update = {
+        $push: {comments: req.body.comment},    // not changing lastEdited here because comments don't feel like an edit
+    };
+    await client.db('daily_bugle').collection('article')
+        .updateOne(filter, update)
+        .then(result => {
+            console.log('updated artice ' + JSON.stringify(filter) + '\nwith changes ' + JSON.stringify(update) + '\nresults: ' + JSON.stringify(result) + '\n');
+            res.send(result);
+        })
+        .catch(err => {
+            console.log('Error updating' + JSON.stringify(filter) + '\ncode: ' + err + '\n');
+            res.status(400).send({
+                message: err
+            });
+        });
+})
