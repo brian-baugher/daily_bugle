d2db0494a42a4be408064c2a2440e26a290d1ad5
Brian Baugher <bdb7hq@virginia.edu>
2024-11-11 23:22:16 -0500

    login flow nicer

--- front/login.js
+++ front/login.js
@@ -5,11 +5,21 @@ getAdToDisplay().then(res => {
 });
 
 const loginForm = document.getElementById('login-form');
+const errorBar = document.getElementById('error-line');
 loginForm.onsubmit = /**@param {SubmitEvent} e */ async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const user = formData.get('username');
     const pass = formData.get('password');
     const role = await submitLogin(user, pass);
-    console.log(role);
+    if(!role){
+        errorBar.innerHTML = "Invalid username or password";
+        return;
+    }
+    else if(role == 'reader'){
+        window.location.replace('http://127.0.0.1:3010/reader.html');
+    }
+    else if(role == 'author'){
+        window.location.replace('http://127.0.0.1:3010/author.html');
+    }
 }
