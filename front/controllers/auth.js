02d30ed3042211281e62ef8c9951da34ccf6b70f
Brian Baugher <bdb7hq@virginia.edu>
2024-11-12 22:48:42 -0500

    can create new users via front end

--- front/controllers/auth.js
+++ front/controllers/auth.js
@@ -26,4 +26,28 @@ const submitLogin = async (username, password) => {
     }
 }
 
-export {submitLogin};
+const createUser = async (username, password) => {
+    try{
+        const res = await fetch(routes.newUser, {
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        console.log(res);
+        if(res.status === 200) {
+            return;
+        } else{
+            return {error: 'problem encountered'};
+        }
+    } catch(err){
+        console.log(err);
+        return {error: err};
+    }
+}
+
+export {submitLogin, createUser};
--- front/login.js
+++ front/login.js
@@ -1,5 +1,5 @@
 import {getAdToDisplay, recordAdImpression} from './controllers/ad.js';
-import { submitLogin } from './controllers/auth.js';
+import { createUser, submitLogin } from './controllers/auth.js';
 getAdToDisplay().then(res => {
     console.log(res);
 });
@@ -13,6 +13,7 @@ loginForm.onsubmit = /**@param {SubmitEvent} e */ async (e) => {
     const pass = formData.get('password');
     const role = await submitLogin(user, pass);
     if(!role){
+        errorBar.style.color = 'red';
         errorBar.innerHTML = "Invalid username or password";
         return;
     }
@@ -21,5 +22,36 @@ loginForm.onsubmit = /**@param {SubmitEvent} e */ async (e) => {
     }
     else if(role == 'author'){
         window.location.replace('http://127.0.0.1:3010/author.html');
+    }
+}
+
+const newUser = document.getElementById('new-user');
+const newModal = document.getElementById('new-modal');
+const content = document.getElementById('content');
+newUser.onclick = () => {
+    content.style.display = 'none';    
+    newModal.style.display = 'block';
+}
+
+const newForm = document.getElementById('new-form');
+const newErr = document.getElementById('new-error')
+newForm.onsubmit = /**@param {SubmitEvent} e */ async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const user = formData.get('username');
+    const password = formData.get('password');
+    const confirm = formData.get('confirmPassword');
+    if(confirm != password){
+        newErr.innerHTML = 'Passwords do not match';
+        return;
+    }
+    const err = await createUser(user, password);
+    if(err){
+        newErr.innerHTML = err;
+    } else{
+        errorBar.innerHTML = "New user created, please log in";
+        errorBar.style.color = 'green';
+        newModal.style.display = 'none';
+        content.style.display = 'block';
     }
 }
